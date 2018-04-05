04c4bd3d1b687fab8bcca59cfa2d93926595d760
Santosh Nalubandhu <snalubandhu3@gatech.edu>
2018-04-05 16:09:00 -0400

    First commit

---
+++ myStore/src/app/app.module.ts
@@ -0,0 +1,38 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import {FormsModule} from '@angular/forms';
+
+
+import { AppComponent } from './app.component';
+import { ProductService } from './shared/product.service';
+import { HomeComponent } from './home/home.component';
+import { FooterComponent } from './footer/footer.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ProductItemComponent } from './product-item/product-item.component';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
+import { SearchComponent } from './search/search.component';
+import {RouterModule} from '@angular/router';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    FooterComponent,
+    NavbarComponent,
+    ProductItemComponent,
+    ProductDetailComponent,
+    SearchComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot([
+      {path: '',                    component: HomeComponent},
+      {path: 'products/:productId', component: ProductDetailComponent}
+    ]),
+  ],
+  providers: [ProductService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
